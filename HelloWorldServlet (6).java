6827447702b3dfd9d35db1247b78b183e5bf579e
Annay Das <annay.das.2004@gmail.com>
2025-07-11 22:10:50 +0600

    Add files via upload

---
+++ HelloWorldServlet (6).java
@@ -0,0 +1,15 @@
+package com.example;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+@WebServlet("/Naimait22001")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World!</h1>");
+    }
+}
